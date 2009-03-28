8f4f1dc0c42e518b89e70b12b75a69b2c5c0e4a7
roa <roa@ordi-ana.petit-chateau>
2009-03-28 20:32:39 +0100

    Translation into English

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
+/*
+* main.cpp
+*
+* Copyright 2009 JMM & Roa <jmimu@free.fr>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+*/
+
+
 /**********************
  *    retouche BD    *
  **********************/
 
-//Nous allons utiliser les sous-librairies suivantes :
-//"cv.h" pour gérer un objet "image",
-//"highgui.h" pour le charger depuis un fichier et l'afficher.
- 
 #include <iostream>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
@@ -17,85 +35,92 @@ using namespace std;
 
 int main()
 {
-	string nom_fich="image_bd2.jpg";
-    IplImage *img=cvLoadImage(nom_fich.c_str());
-    IplImage *img_nvg = cvCreateImage(cvGetSize(img), img->depth, 1);
-    IplImage *img_sortie = cvCloneImage(img);
+	string filename="image_bd2.jpg";
+    IplImage *img=cvLoadImage(filename.c_str());
+    IplImage *img_gray = cvCreateImage(cvGetSize(img), img->depth, 1);
+    IplImage *img_output = cvCloneImage(img);
     
-    //Convertir en niveaux de gris :
-	cvConvertImage(img, img_nvg);
+    //Convert into grayscale
+	cvConvertImage(img, img_gray);
 
-    //Creation de img_canny : même taille que img, même profondeur aussi, mais 1 canal
-	IplImage *img_canny = cvCreateImage(cvGetSize(img_nvg), img_nvg->depth, 1);
+    //Creation of img_canny: same size and depth as img_gray, but only one channel
+	IplImage *img_canny = cvCreateImage(cvGetSize(img_gray), img_gray->depth, 1);
         
-    cvCanny(img_nvg, img_canny,500,300); //******CANNY - DETECTION DES BORDS
+    cvCanny(img_gray, img_canny,500,300); //******CANNY - EDGES DETECTION
     
-    //Creation d'une image - on garde que le contour exterieur
-    IplImage *img_ext = cvCreateImage(cvGetSize(img_nvg), img_nvg->depth, 1);
+    //Creation of img_ext in which we keep only the external edges
+    IplImage *img_ext = cvCreateImage(cvGetSize(img_gray), img_gray->depth, 1);
     
-    CvScalar scalaire; //scalaire - l'endroit ou on va stocker un pixel
+    CvScalar scalar; //scalar -  place where we stock the pixel
 	
-	//initialise l'image en noir (a ameliorer !)
+	//Initialize the image in black (TO IMPROVE!!!!!!!)
 	for(int y=0; y<img_canny->height; y++)
 	{
     	for(int x=0; x<img_canny->width; x++)
     	{
-            //On recupere le pixel (x,y) de l'image img_canny.
-        	scalaire=cvGet2D(img_canny, y, x);
-            scalaire.val[0]=0;//on met tous les pixels en noir
-        	cvSet2D(img_ext, y, x, scalaire);
+            //Get the pixel (x,y) from img_canny
+        	scalar=cvGet2D(img_canny, y, x);
+            scalar.val[0]=0;//pixels - all black
+        	cvSet2D(img_ext, y, x, scalar);
         }
 	}
 	
-	//on ne garde que le premier et le dernier pixel allumes pour chaque ligne
-	bool trouve_premier, trouve_dernier;
+	//Only the first and the last pixels lit (true) in each line
+	bool found_first, found_last;
 	for(int y=0; y<img_canny->height; y++)
 	{
-		trouve_premier=false;
-		trouve_dernier=false;
+		found_first=false;
+		found_last=false;
     	for(int x=0; x<img_canny->width; x++)
     	{
-            //On recupere le pixel (x,y) de l'image img_canny.
-        	scalaire=cvGet2D(img_canny, y, x);
-            //on cherche le premier pixel pas noir (>0)
-        	if ((scalaire.val[0]>0) && (!trouve_premier)) //si le pixel n'est pas noir et si le premier pas noir n'a pas ete trouve
+            //Get the (x,y) pixel from img_canny
+        	scalar=cvGet2D(img_canny, y, x);
+            //Search the first not black pixel (>0)
+        	if ((scalar.val[0]>0) && (!found_first)) //if the pixel is not black and if the first not black pixel was not found yet
         	{
-        		cvSet2D(img_ext, y, x, scalaire);
-        		trouve_premier=true;
+        		cvSet2D(img_ext, y, x, scalar);
+        		found_first=true;
         	}
-        	scalaire=cvGet2D(img_canny, y,img_canny->width-x-1);
-            //on cherche le dernier pixel pas noir (>0)
-        	if ((scalaire.val[0]>0)&& (!trouve_dernier))
+        	scalar=cvGet2D(img_canny, y,img_canny->width-x-1);
+            ////Search the last not black pixel (>0)
+        	if ((scalar.val[0]>0)&& (!found_last)) //if the pixel is not black and if the last not black pixel was not found yet
         	{
-        		cvSet2D(img_ext, y, img_canny->width-x-1, scalaire);
-        		trouve_dernier=true;
+        		cvSet2D(img_ext, y, img_canny->width-x-1, scalar);
+        		found_last=true;
         	}
-        	if ((trouve_premier) && (trouve_dernier)) break;
+        	if ((found_first) && (found_last)) break;
 
     	}
 	}
 	
-	//recherche de lignes par transformee de Hough 
+	
+	cvNamedWindow("Retouche BD", 1);
+    cvShowImage("Retouche BD", img_ext);
+    cvWaitKey(0);
+    cvDestroyWindow("Retouche BD");
+    
+	
+	//Search lines using Hough transformation
 	CvMemStorage* storage = cvCreateMemStorage(0);
 	CvSeq* lines = 0;
-	lines = cvHoughLines2(img_ext, storage, CV_HOUGH_PROBABILISTIC, 0.1, CV_PI/180/20, 50, 200, 50 );
-	cout<<"Nombre de lignes trouvees  : "<<lines->total<<endl;
+	lines = cvHoughLines2(img_ext, storage, CV_HOUGH_PROBABILISTIC, 0.1, CV_PI/180/20, 20, 20, 10 );
+	cout<<"Number of found lines: "<<lines->total<<endl;
 	
-	double moy_theta=0;
+	double mean_theta=0;
 	for( int i = 0; i < lines->total; i++ )
 	{
 		CvPoint* line = (CvPoint*)cvGetSeqElem(lines,i);
-		cvLine( img_sortie, line[0], line[1], CV_RGB(255,0,0), 13, 8 );
+		cvLine( img_output, line[0], line[1], CV_RGB(255,0,0), 13, 8 );
 		double theta = atan ((line[1].x-line[0].x) / (double)(line[1].y-line[0].y))*180/CV_PI;
-		moy_theta+=theta;
-		cout<<"    ligne numero "<<i<<"   theta "<<theta<<endl;
+		mean_theta+=theta;
+		cout<<"    Line number "<<i<<"   theta "<<theta<<endl;
 	}
-	moy_theta/=lines->total;
-	cout<<"theta moyen "<<moy_theta<<endl;
+	mean_theta/=lines->total;
+	cout<<"Mean orientation: "<<mean_theta<<endl;
  
- 	//appel imagemagick
+ 	//Call imagemagick
  	ostringstream oss;
- 	oss<<"convert "<<nom_fich<<" -rotate "<<moy_theta<<" _"<<nom_fich;
+ 	oss<<"convert "<<filename<<" -rotate "<<mean_theta<<" _"<<filename;
 	system(oss.str().c_str());
  	
  	cvLine( img, cvPoint(0,0), cvPoint(img->width,0), CV_RGB(255,255,255));
@@ -103,133 +128,130 @@ int main()
  	cvLine( img, cvPoint(0,img->height), cvPoint(img->width,img->height), CV_RGB(255,255,255));
  	cvLine( img, cvPoint(img->width,0), cvPoint(img->width,img->height), CV_RGB(255,255,255));
 
- 	IplImage *img_tourne = cvCreateImage(cvGetSize(img), img->depth, 3);
+ 	IplImage *img_turned = cvCreateImage(cvGetSize(img), img->depth, 3);
  	float m[6];
-	double factor = 1;//(cos(moy_theta*CV_PI/180.) + 1.1)*3;
+	double factor = 1;//(cos(mean_theta*CV_PI/180.) + 1.1)*3;
 	CvMat M = cvMat( 2, 3, CV_32F, m );
 	int w = img->width;
 	int h = img->height;
-	m[0] = (float)(factor*cos(moy_theta*CV_PI/180.));
-	m[1] = (float)(factor*sin(moy_theta*CV_PI/180.));
+	m[0] = (float)(factor*cos(mean_theta*CV_PI/180.));
+	m[1] = (float)(factor*sin(mean_theta*CV_PI/180.));
 	m[2] = w*0.5f;
 	m[3] = -m[1];
 	m[4] = m[0];
 	m[5] = h*0.5f;
-	cvGetQuadrangleSubPix( img, img_tourne, &M);//, 1, cvScalarAll(0));
+	cvGetQuadrangleSubPix( img, img_turned, &M);//, 1, cvScalarAll(0));
  	
- 	cvSaveImage("toto.jpg",img_tourne);
+ 	cvSaveImage("toto.jpg",img_turned);
  	
 //-------------------------------------------------------------------------------------
- 	IplImage *img_nvg_tourne = cvCreateImage(cvGetSize(img_tourne), img_tourne->depth, 1);
-    cvConvertImage(img_tourne, img_nvg_tourne);
- 	//Creation de img_canny_tourne : meme taille que img_tourne, meme profondeur aussi, mais 1 canal
-	IplImage *img_canny_tourne = cvCreateImage(cvGetSize(img_nvg_tourne), img_nvg_tourne->depth, 1);
-    cvCanny(img_nvg_tourne, img_canny_tourne,500,300); //******CANNY - DETECTION DES BORDS
+ 	IplImage *img_gray_turned = cvCreateImage(cvGetSize(img_turned), img_turned->depth, 1);
+    cvConvertImage(img_turned, img_gray_turned);
+ 	//Creation of img_canny_turned : same size and depth as img_gray, but only one channel
+	IplImage *img_canny_turned = cvCreateImage(cvGetSize(img_gray_turned), img_gray_turned->depth, 1);
+    cvCanny(img_gray_turned, img_canny_turned,500,300); //******CANNY - EDGES DETECTION
 
     /*cvNamedWindow("Retouche BD", 1);
-    cvShowImage("Retouche BD", img_canny_tourne);
+    cvShowImage("Retouche BD", img_canny_turned);
     cvWaitKey(0);
     cvDestroyWindow("Retouche BD");
     */
     
-	//on ne garde que le premier pixel allume en y
-	int coord_bord_haut=-1;
-	for(int y=0; y<img_canny_tourne->height; y++)
+	//Keep only the first lit pixel in y
+	int top_side_coord=-1;
+	for(int y=0; y<img_canny_turned->height; y++)
 	{
-		for(int x=0; x<img_canny_tourne->width; x++)
+		for(int x=0; x<img_canny_turned->width; x++)
 		{
-			//On recupere le pixel (x,y) de l'image img_canny_tourne
-			scalaire=cvGet2D(img_canny_tourne, y, x);
-			//on cherche le premier pixel pas noir (>0)
-			if (scalaire.val[0]>0) //si le pixel n'est pas noir
+			//Get the (x,y) pixel from img_canny_turned
+			scalar=cvGet2D(img_canny_turned, y, x);
+			//Search the first not black pixel in y
+			if (scalar.val[0]>0) 
 			{
-				coord_bord_haut=y-1;
+				top_side_coord=y-1;
 				break;
 			}
 		}
-		if (coord_bord_haut>-1) break;
+		if (top_side_coord>-1) break;
 	}
-    cout<<"bord haut : "<<coord_bord_haut<<"\n";
+    cout<<"y top side: "<<top_side_coord<<"\n";
 	//on connait maintenant le y du bord haut de l'image
  	
-
- 	//on ne garde que le dernier pixel allume en y
-	int coord_bord_bas=img_canny_tourne->height;
-	for(int y=img_canny_tourne->height-1;y>=0; y--)
+	//Keep only the last lit pixel in y
+ 	int bottom_side_coord=img_canny_turned->height;
+	for(int y=img_canny_turned->height-1;y>=0; y--)
 	{
-		for(int x=0; x<img_canny_tourne->width; x++)
+		for(int x=0; x<img_canny_turned->width; x++)
 		{
-			//On recupere le pixel (x,y) de l'image img_canny_tourne
-			scalaire=cvGet2D(img_canny_tourne, y, x);
-			//on cherche le premier pixel pas noir (>0)
-			if (scalaire.val[0]>0) //si le pixel n'est pas noir
+			//Get the (x,y) pixel from img_canny_turned
+			scalar=cvGet2D(img_canny_turned, y, x);
+			//Search the last not black pixel in y
+			if (scalar.val[0]>0) 
 			{
-				coord_bord_bas=y+1;
+				bottom_side_coord=y+1;
 				break;
 			}
 		}
-		if (coord_bord_bas<img_canny_tourne->height) break;
+		if (bottom_side_coord<img_canny_turned->height) break;
 	}
-    cout<<"bord bas : "<<coord_bord_bas<<"\n";
-	//on connait maintenant le y du bord bas de l'image
+    cout<<"y bottom side: "<<bottom_side_coord<<"\n";
 	
-	//on ne garde que le premier pixel allume en x
-	int coord_bord_gauche=-1;
-    for(int x=0; x<img_canny_tourne->width; x++)
+	
+	//Keep only the first lit pixel in x
+	int left_side_coord=-1;
+    for(int x=0; x<img_canny_turned->width; x++)
 	{
-        for(int y=0;y<img_canny_tourne->height;y++)
+        for(int y=0;y<img_canny_turned->height;y++)
 		{
-			//On recupere le pixel (x,y) de l'image img_canny_tourne
-			scalaire=cvGet2D(img_canny_tourne, y, x);
-			//on cherche le premier pixel pas noir (>0)
-			if (scalaire.val[0]>0) //si le pixel n'est pas noir
+			//Get the (x,y) pixel from img_canny_turned
+			scalar=cvGet2D(img_canny_turned, y, x);
+			//Search the first not black pixel in x
+			if (scalar.val[0]>0) 
 			{
-				coord_bord_gauche=x-1;
+				left_side_coord=x-1;
 				break;
 			}
 		}
-		if (coord_bord_gauche>-1) break;
+		if (left_side_coord>-1) break;
 	}
-    cout<<"bord gauche : "<<coord_bord_gauche<<"\n";
-	//on connait maintenant le x du bord gauche de l'image
-	
-    //on ne garde que le dernier pixel allume en x
-	int coord_bord_droite=img_canny_tourne->width;
-    for(int x=img_canny_tourne->width-1;x>=0; x--)
+    cout<<"x left side: "<<left_side_coord<<"\n";
+		
+    //Keep only the last lit pixel in y
+	int right_side_coord=img_canny_turned->width;
+    for(int x=img_canny_turned->width-1;x>=0; x--)
 	{
-        for(int y=0;y<img_canny_tourne->height;y++)
+        for(int y=0;y<img_canny_turned->height;y++)
 		{
-			//On recupere le pixel (x,y) de l'image img_canny_tourne
-			scalaire=cvGet2D(img_canny_tourne, y, x);
-			//on cherche le premier pixel pas noir (>0)
-			if (scalaire.val[0]>0) //si le pixel n'est pas noir
+			//Get the (x,y) pixel from img_canny_turned
+			scalar=cvGet2D(img_canny_turned, y, x);
+			//Search the last not black pixel in x
+			if (scalar.val[0]>0) 
 			{
-				coord_bord_droite=x+1;
+				right_side_coord=x+1;
 				break;
 			}
 		}
-		if (coord_bord_droite<img_canny_tourne->width) break;
+		if (right_side_coord<img_canny_turned->width) break;
 	}
-    cout<<"bord droite : "<<coord_bord_droite<<"\n";
-	//on connait maintenant le x du bord droit de l'image
+    cout<<"x right side: "<<right_side_coord<<"\n";
 	
 	
-	IplImage *img_sortie_tourne = cvCloneImage(img_tourne);
-	cvLine( img_sortie_tourne, cvPoint(0,coord_bord_haut), cvPoint(img->width,coord_bord_haut), CV_RGB(0,0,255),1);
-	cvLine( img_sortie_tourne, cvPoint(0,coord_bord_bas), cvPoint(img->width,coord_bord_bas), CV_RGB(0,0,255),1);
-	cvLine( img_sortie_tourne, cvPoint(coord_bord_gauche,0), cvPoint(coord_bord_gauche,img->height), CV_RGB(0,0,255),1);
-	cvLine( img_sortie_tourne, cvPoint(coord_bord_droite,0), cvPoint(coord_bord_droite,img->height), CV_RGB(0,0,255),1);
+	IplImage *img_output_turned = cvCloneImage(img_turned);
+	cvLine( img_output_turned, cvPoint(0,top_side_coord), cvPoint(img->width,top_side_coord), CV_RGB(0,0,255),1);
+	cvLine( img_output_turned, cvPoint(0,bottom_side_coord), cvPoint(img->width,bottom_side_coord), CV_RGB(0,0,255),1);
+	cvLine( img_output_turned, cvPoint(left_side_coord,0), cvPoint(left_side_coord,img->height), CV_RGB(0,0,255),1);
+	cvLine( img_output_turned, cvPoint(right_side_coord,0), cvPoint(right_side_coord,img->height), CV_RGB(0,0,255),1);
  	
 
     cvNamedWindow("Retouche BD", 1);
-    cvShowImage("Retouche BD", img_sortie_tourne);
+    cvShowImage("Retouche BD", img_output_turned);
     cvWaitKey(0);
     cvDestroyWindow("Retouche BD");
     
     
-    //Libération de l'IplImage (on lui passe un IplImage**).
+    //Release the IplImage (give to cvReleaseImage an IplImage** parameter).
     cvReleaseImage(&img);
- 	cvReleaseImage(&img_nvg);
+ 	cvReleaseImage(&img_gray);
     cvReleaseImage(&img_canny);
     cvReleaseImage(&img_ext);
  	
